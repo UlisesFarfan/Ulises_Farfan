bbd524c8adeede179b5c2444dd1ebb5c09679ca4
UlisesFarfan <ulisesfarfan296@gmail.com>
2023-03-17 19:48:00 -0300

    fix data

--- src/data/works.js
+++ src/data/works.js
@@ -3,7 +3,7 @@ const works = [
     title: 'Ontiime - Full Stack Developer',
     date: '12/2022 - 2/2023',
     description: false,
-    techs: 'Working whith TypeScript, React, Node.js, Express.js, MongoDB and SCSS.'
+    techs: 'TypeScript, React, Node.js, Express.js, MongoDB and SCSS.'
   },
   {
     title: 'Kodengo - Full Stack Developer',
