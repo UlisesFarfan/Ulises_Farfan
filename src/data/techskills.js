9c5de3ab4cb5bf8ce1f3a7cea4a89feb5f2d6f7c
Ulises Farfan <94791119+UlisesFarfan@users.noreply.github.com>
2023-07-31 19:26:55 -0300

    Update techskills.js

--- src/data/techskills.js
+++ src/data/techskills.js
@@ -7,8 +7,8 @@ const techSkills = [
     {
         name: 'TypeScript',
         icon: TbBrandTypescript,
-        seniority: 'Junior',
-        experience: '5 months working',
+        seniority: '',
+        experience: '',
     },
     {
         name: 'JavaScript',
