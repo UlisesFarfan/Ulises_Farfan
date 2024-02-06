d5afa0ea4bc66fb81d20ca4bbf9ac8bfb927d8c9
UlisesFarfan <ulisesfarfan296@gmail.com>
2024-02-06 15:09:36 -0300

    fix

--- src/data/works.js
+++ src/data/works.js
@@ -1,6 +1,6 @@
 const works = [
   {
-    title: 'Bitseat: Full Stack Web & Mobile Developer - November 2023 to Actual',
+    title: 'Bitseat: Full Stack Web & Mobile Developer - November 2023 to Actually',
     date: '',
     description: false,
     techs: 'TypeScript, JavaScript, React, Node.js, Express.js, Serverless, AWS, SCSS, Flutter, Dart.'
